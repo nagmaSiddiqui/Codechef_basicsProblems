79fc265e0765a70105a1fb42481d3f64cde6a470
nagmaSiddiqui <106270858+nagmaSiddiqui@users.noreply.github.com>
2023-05-30 00:21:26 +0530

    Add files via upload

---
+++ Squates.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int X;
+        cin>>X;
+        int Y=15*X;
+        cout<<Y<<endl;
+    }
+    
+
+	return 0;
+}
+
