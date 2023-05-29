278f0258cfe74876a69721f30a09531b6fe02739
nagmaSiddiqui <106270858+nagmaSiddiqui@users.noreply.github.com>
2023-05-30 00:28:48 +0530

    Add files via upload

---
+++ Saving Taxes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int X,Y;
+	    cin>>X>>Y;
+	    int c=X-Y;
+	    cout<<c<<endl;
+	}
+	return 0;
+}
+
